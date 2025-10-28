035f7206c2fc93e89a8a39c30ee5938583b9513d
Swarupa2327 <swaruswaru83@gmail.com>
2025-10-28 22:09:50 +0530

    react app deploying Reactapp

---
+++ task5/src/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer(){
+    return(
+        <div id="ftr">
+           <div id="imgs">
+            <img src="https://tse2.mm.bing.net/th/id/OIP.xt9mDGdlVGzWlGytw43gzQHaHa?pid=Api&P=0&h=180"  />
+            <img src="https://tse3.mm.bing.net/th/id/OIP.xBjedU7TqMZjjeGHW6lTIwHaHa?pid=Api&P=0&h=180"  />
+            <img src="https://tse2.mm.bing.net/th/id/OIP.FTsmhcqVyz0W113VVYRFpAAAAA?pid=Api&P=0&h=180"  />
+            <img src="https://tse3.mm.bing.net/th/id/OIP.SiyJFWtdVpNADi-J0Gm1eAHaHa?pid=Api&P=0&h=180"  />
+            </div>
+            <p style={{textAlign:"center"}}>© 2023 Freeeze. All Rights Reserved.</p>
+        </div>
+    )
+}
+export default Footer;
